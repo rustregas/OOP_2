491cc7fc22f9c94132f54da58193479547dac646
Rustam Yusupov <yusupov@gnivc.ru>
2022-05-03 17:31:44 +0300

    first_commit

---
+++ src/pro/sky/java/course2/Bicycle.java
@@ -0,0 +1,11 @@
+package pro.sky.java.course2;
+
+public class Bicycle {
+
+    public String modelName;
+    public int wheelsCount;
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+}
