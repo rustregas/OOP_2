990423c5498db8f765e0a1a9bf5b5110b1a258b8
Rustam Yusupov <yusupov@gnivc.ru>
2022-05-03 18:19:40 +0300

    hw_oop_2

--- src/pro/sky/java/course2/ServiceStation.java
+++ src/pro/sky/java/course2/ServiceStation.java
@@ -1,25 +1,28 @@
 package pro.sky.java.course2;
 
 public class ServiceStation {
-    public void check(Car car, Bicycle bicycle, Truck truck) {
-        if (car != null) {
-            System.out.println("Обслуживаем " + car.modelName);
-            for (int i = 0; i < car.wheelsCount; i++) {
-                car.updateTyre();
-            }
-            car.checkEngine();
-        } else if (truck != null) {
-            System.out.println("Обслуживаем " + truck.modelName);
-            for (int i = 0; i < truck.wheelsCount; i++) {
-                truck.updateTyre();
-            }
-            truck.checkEngine();
-            truck.checkTrailer();
-        } else if (bicycle != null) {
-            System.out.println("Обслуживаем " + bicycle.modelName);
-            for (int i = 0; i < bicycle.wheelsCount; i++) {
-                bicycle.updateTyre();
-            }
+
+    private static void updateTyre(Vehicle vhcl) {
+        for (int i = 0; i < vhcl.getWheelsCount(); i++) {
+            vhcl.updateTyre();
         }
+    }
+
+    public void check(Bicycle bicycle){
+        System.out.println("Обслуживаем " + bicycle.getModelName());
+        updateTyre(bicycle);
+    }
+
+    public void check(Car car){
+        System.out.println("Обслуживаем " + car.getModelName());
+        updateTyre(car);
+        car.checkEngine();
+    }
+
+    public void check(Truck truck){
+        System.out.println("Обслуживаем " + truck.getModelName());
+        updateTyre(truck);
+        truck.checkEngine();
+        truck.checkTrailer();
     }
 }
